db45014e8d149f105def11afb712e73f02e52244
valentin_usatiuk <117016618+ValentinUsatiuk@users.noreply.github.com>
2023-08-19 23:22:22 +0300

    Add functionality in Hotel class and  in ReservationTicket class

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 import pandas
 
-df = pandas.read_csv("hotels.csv")
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
 
 
 class Hotel:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
 
     def book(self):
-        pass
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
-        pass
+        """Check if hotel is available"""
+        avialabilyty = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if avialabilyty == "yes":
+            return True
+        else:
+            return False
 
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
-        self.name = customer_name
+        self.customer_name = customer_name
         self.hotel = hotel_object
 
     def generate(self):
-        content = f"Name of the customer hotel"
+        content = f"""
+        Thank you for your reservation!
+        Here are you booking data:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
         return content
 
 
 print(df)
-id = input("Enter the id of the hotel: ")
-hotel = Hotel(id)
+hotel_ID = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
