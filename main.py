0503b6596b12ab49e84da8e6cb8a3149eb7e18ff
valentin_usatiuk <117016618+ValentinUsatiuk@users.noreply.github.com>
2023-08-19 22:43:14 +0300

    Write a simple project structure using classes without functionality

--- main.py
+++ main.py
@@ -10,12 +10,27 @@ class Hotel:
     def book(self):
         pass
 
-
-class ReservationTicket:
-    def generate(self):
+    def available(self):
         pass
 
 
+class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.name = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"Name of the customer hotel"
+        return content
+
+
 print(df)
-id = input("Enter the id of the hotel")
+id = input("Enter the id of the hotel: ")
 hotel = Hotel(id)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("Hotel is not free")
